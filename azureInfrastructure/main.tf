3ada2568e2fbed02e2b5371ab34ad3aedb1c854a
Stevan Thomas <stevansunny@gmail.com>
2024-01-02 15:00:04 -0500

    reverted to previous method

--- azureInfrastructure/main.tf
+++ azureInfrastructure/main.tf
@@ -6,19 +6,14 @@ terraform {
     }
   }
 }
-variable "creds" {
-  default = file("mycredentials.json")
-}
-
-
 
 provider "azurerm" {
   features {}
 
-  subscription_id   = var.creds.ARM_SUBSCRIPTION_ID
-  tenant_id         = var.creds.ARM_TENANT_ID
-  client_id         = var.creds.ARM_CLIENT_ID
-  client_secret     = var.creds.ARM_CLIENT_SECRET
+  subscription_id   = var.ARM_SUBSCRIPTION_ID
+  tenant_id         = var.ARM_TENANT_ID
+  client_id         = var.ARM_CLIENT_ID
+  client_secret     = var.ARM_CLIENT_SECRET
 }
 
 
