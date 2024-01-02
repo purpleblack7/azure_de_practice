b81efb5a5e5fcdd29f1a5c86a3fa9561b8c56e63
Stevan Thomas <stevansunny@gmail.com>
2024-01-02 13:57:34 -0500

    Initial build to create rg, storage accounts and blobs

--- azureInfrastructure/main.tf
+++ azureInfrastructure/main.tf
@@ -10,25 +10,25 @@ terraform {
 provider "azurerm" {
   features {}
 
-  subscription_id   = "<azure_subscription_id>"
-  tenant_id         = "<azure_subscription_tenant_id>"
-  client_id         = "<service_principal_appid>"
-  client_secret     = "<service_principal_password>"
+  subscription_id   = var.ARM_SUBSCRIPTION_ID
+  tenant_id         = var.ARM_TENANT_ID
+  client_id         = var.ARM_CLIENT_ID
+  client_secret     = var.ARM_CLIENT_SECRET
 }
 
 
 # Create a resource group
-resource "azurerm_resource_group" "primary" {
+resource "azurerm_resource_group" "rg" {
     name = "de_demo_rg"
     location = "eastus"
 }
 
 
 #Create an Azure Storage Account
-resource azurerm_storage_account "primary" {
+resource azurerm_storage_account "store" {
     name = "de_demo_storage"
-    resource_group_name = azurerm_resource_group.primary.name
-    location = azurerm_resource_group.primary.location
+    resource_group_name = azurerm_resource_group.rg.name
+    location = azurerm_resource_group.rg.location
     account_kind = "StorageV2"
     account_tier = "Standard"
     account_replication_type = "LRS"
@@ -36,7 +36,8 @@ resource azurerm_storage_account "primary" {
 
 #Create a Blob Container within Storage Account
 
-resource azurerm_storage_container "datasetsInitial" {
-    name = "datasetsInitial"
-    stor
+resource azurerm_storage_container "blob" {
+    name = "content"
+    storage_account_name = azurerm_resource_group.store.name
+    container_access_type = 'private"
 }
